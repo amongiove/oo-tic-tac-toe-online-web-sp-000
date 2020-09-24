4bbe1416b9a7619edfdb72c4c77d438b7bf621e8
Amanda Mongiove <amandamongiove@gmail.com>
2020-09-24 17:12:33 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -4,10 +4,11 @@ class TicTacToe
 
   def initialize
     @board = ([" "," "," "," "," "," "," "," "," "])
+    @board = board
   end
 
 
-  def display_board(@board)
+  def display_board(board)
     puts " #{board[0]} | #{board[1]} | #{board[2]} "
     puts "-----------"
     puts " #{board[3]} | #{board[4]} | #{board[5]} "
